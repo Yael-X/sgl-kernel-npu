179cd910ae2bd6a8b0e5ce78eaa9638777fbfdac
XuYue <x00575210@china.huawei.com>
2025-07-24 22:59:48 +0800

    bugfix

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ set -e
 CURRENT_DIR=$(pwd)
 PYTHON_DIR="$CURRENT_DIR"/python
 
-# Remove old dist file, build, and install
-bash ./build.sh
-
 cd $PYTHON_DIR
 rm -rf dist
 python setup.py bdist_wheel
